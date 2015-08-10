82d1a38dfa13be40480b930dcebc01decf978e65
Lawrence Buss <LawSBuss@gmail.com>
2015-08-10 12:46:05 -0700

    dummy module started

---
+++ dummy_module.py
@@ -0,0 +1,17 @@
+info = {
+    'version': .1,
+    'author': 'Lawrence'
+}
+
+inputs = ['CNC']
+outputs = ['OMG', 'BTT']
+
+def run(CNC):
+    print(CNC)
+    omg = 'husk'
+    btt = 'blet'
+    
+    return {
+        'OMG': omg,
+        'BTT': btt
+    }
